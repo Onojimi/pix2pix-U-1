f5aeb0cb5575dc1179270e2c5504d8e9326b2244
Onojimi <18811505578@163.com>
2019-08-22 15:29:05 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,6 +17,7 @@ def save_img(image_tensor, filename):
     print(torch.unique(image_tensor))
     image_numpy = image_tensor.float().numpy()
     np.where(image_numpy>0, 1, 0)
+    image_numpy  = np.transpose(image_numpy, (1, 2, 0))
 #    image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 
 #    image_numpy = image_numpy.clip(0, 1)
 #    image_numpy = image_numpy.astype(np.uint8)
