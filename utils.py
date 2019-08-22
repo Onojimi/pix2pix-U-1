4722a0c0042b09f7a60b8c84e21031bd1c5eb7cd
Onojimi <18811505578@163.com>
2019-08-22 15:29:59 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,6 +18,7 @@ def save_img(image_tensor, filename):
     image_numpy = image_tensor.float().numpy()
     np.where(image_numpy>0, 1, 0)
     image_numpy  = np.transpose(image_numpy, (1, 2, 0))
+    print(image_numpy.shape)
 #    image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 
 #    image_numpy = image_numpy.clip(0, 1)
 #    image_numpy = image_numpy.astype(np.uint8)
