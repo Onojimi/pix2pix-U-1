0ef4c1d5881f3e3a4d078c0c45a9f3fbad51cdbb
Onojimi <18811505578@163.com>
2019-08-24 14:44:08 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import argparse
 import os
 
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 
@@ -10,8 +11,8 @@ from utils import is_image_file, load_img, save_img
 
 # Testing settings
 parser = argparse.ArgumentParser(description='pix2pix-pytorch-implementation')
-parser.add_argument('--dataset', required=True, help='facades')
-parser.add_argument('--direction', type=str, default='b2a', help='a2b or b2a')
+#which dataset to use
+#parser.add_argument('--dataset', required=True, help='facades')
 parser.add_argument('--nepochs', type=int, default=200, help='saved model of which epochs')
 parser.add_argument('--cuda', action='store_true', help='use cuda')
 opt = parser.parse_args()
@@ -19,15 +20,17 @@ print(opt)
 
 device = torch.device("cuda:0" if opt.cuda else "cpu")
 
-model_path = "checkpoint/{}/netG_model_epoch_{}.pth".format(opt.dataset, opt.nepochs)
+#where the model is stored
+#model_path = "checkpoint/{}/netG_model_epoch_{}.pth".format(opt.dataset, opt.nepochs)
+model_path = "netG_model_epoch_{}.pth".format(opt.nepochs)
 
 net_g = torch.load(model_path).to(device)
+net_g = nn.DataParallel(net_g,device_ids=[0,1,2])
 
+#image_dir = "dataset/{}/test/images/".format(opt.dataset)
+image_pre_dir = "/media/hdd1/MUTTER_WSI/patches_sn/"
 
-image_dir = "dataset/{}/test/images/".format(opt.dataset)
-
-
-image_filenames = [x for x in os.listdir(image_dir) if is_image_file(x)]
+image_filenames = [x for x in os.listdir(image_pre_dir) if is_image_file(x)]
 
 transform_list = [transforms.ToTensor(),
                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
@@ -35,12 +38,15 @@ transform_list = [transforms.ToTensor(),
 transform = transforms.Compose(transform_list)
 
 for image_name in image_filenames:
-    img = load_img(image_dir + image_name)
+    img = load_img(image_pre_dir + image_name)
     img = transform(img)
     input = img.unsqueeze(0).to(device)
     out = net_g(input)
     out_img = out.detach().squeeze(0).cpu()
 
-    if not os.path.exists(os.path.join("result", opt.dataset)):
-        os.makedirs(os.path.join("result", opt.dataset))
-    save_img(out_img, "result/{}/{}".format(opt.dataset, image_name))
+#     if not os.path.exists(os.path.join("result", opt.dataset)):
+#         os.makedirs(os.path.join("result", opt.dataset))
+#     save_img(out_img, "result/{}/{}".format(opt.dataset, image_name))
+    if not os.path.exists(os.path.join(image_pre_dir, "result")):
+        os.makedirs(os.path.join(image_pre_dir, "result"))
+    save_img(out_img, image_pre_dir+"result/{}".format(opt.dataset, image_name))
